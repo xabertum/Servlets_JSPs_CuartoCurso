382da625d9e38d467e3d002d70347e7ca0926a07
xabertum <mohatar@gmail.com>
2019-08-09 20:24:12 +0200

    añadida validacion JS

--- WebContent/resources/funciones.js
+++ WebContent/resources/funciones.js
@@ -1,5 +1,14 @@
 /**
  * 
  */
+function validarForma(forma) {
 
+	var usuario = forma.usuario;
+	if (usuario.value == "" || usuario.value =="Escribir usuario") {
+		alert("Debe introducir un usario valido")
+	}
+
+
+
+}
  
