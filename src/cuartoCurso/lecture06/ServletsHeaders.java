055386ddd52112bbf3b0d4cf5ab60c71e3e311cf
xabertum <mohatar@gmail.com>
2019-08-12 19:49:51 +0200

    minor format changes

--- src/cuartoCurso/lecture06/ServletsHeaders.java
+++ src/cuartoCurso/lecture06/ServletsHeaders.java
@@ -15,44 +15,44 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletsHeaders")
 public class ServletsHeaders extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletsHeaders() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public ServletsHeaders() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		response.setContentType("text/html;charset=UTF-8");
-		
+
 		try {
 			String metodoHttpString = request.getMethod();
 			String uriString = request.getRequestURI();
-			
-			response.getWriter().append("El metodo HTTP es: " + metodoHttpString + "/n");
-			response.getWriter().append("EL URI solicitado es: " + uriString + "/n");
-			response.getWriter().append("Host" + request.getHeader("Host") + "/n");
-			response.getWriter().append("Navegador: " + request.getHeader("User-Agent") + "/n");
-			
+
+			response.getWriter().append("El metodo HTTP es: " + metodoHttpString + "\n");
+			response.getWriter().append("EL URI solicitado es: " + uriString + "\n");
+			response.getWriter().append("Host" + request.getHeader("Host") + "\n");
+			response.getWriter().append("Navegador: " + request.getHeader("User-Agent") + "\n");
+
 			Enumeration<?> cabecerosEnumeration = request.getHeaderNames();
 			while (cabecerosEnumeration.hasMoreElements()) {
 				String nombreCabecero = (String) cabecerosEnumeration.nextElement();
-				response.getWriter().append("<b>" + nombreCabecero + "</b>" +"/n");
-				response.getWriter().append(request.getHeader(nombreCabecero) +"/n");				
-			}			
-			
+				response.getWriter().append("<b>" + nombreCabecero + "</b>" + "\n");
+				response.getWriter().append(request.getHeader(nombreCabecero) + "\n");
+			}
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-	}
 
-	
+	}
 
 }
