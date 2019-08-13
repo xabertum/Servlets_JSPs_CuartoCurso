f9f37779a12e46998ce9913bd315d7fe32847e17
xabertum <mohatar@gmail.com>
2019-08-13 11:12:16 +0200

    Work continued

--- src/cuartoCurso/lecture10/ManejoCookies/CookiesServlet.java
+++ src/cuartoCurso/lecture10/ManejoCookies/CookiesServlet.java
@@ -1,8 +1,11 @@
 package cuartoCurso.lecture10.ManejoCookies;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,29 +16,56 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CookiesServlet")
 public class CookiesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CookiesServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public CookiesServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Suponemos que el usuario está visitando por primera vez nuestro sitio
+		boolean nuevoUsuario = true;
+
+		// Obtenemos todas las cookies
+		Cookie[] cookies = request.getCookies();
+
+		// Buscamos si ya existe una cookie creada con anterioridad llamada
+		// visitanteRecurrente
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("visitanteRecurrente") && cookie.getValue().equals("si")) {
+					// En caso de ya exista una cookie indicando que ya existe un usuario registrado
+					// con anterioridad, ponemos la bandera en falso y salimos del ciclo
+					nuevoUsuario = false;
+					break;
+				}
+			}
+		}
+
+		// Revisamos si el el usuario es nuevo visitante
+		String mensajeString = null;
+		if (nuevoUsuario) {
+			// En caso de ser un usuario nuevo creamos el objeto cookie
+			Cookie visitanteCookie = new Cookie("visitanteRecurrente", "si");
+			// Agregamos la cookie en la respuesta
+			response.addCookie(visitanteCookie);
+			mensajeString = "Gracias por visitar nuestro sitio";
+
+		} else {
+			mensajeString = "Gracias por visitar NUEVAMENTE nuestro sitio";
+		}
+
+		// Escribimos la salida
+		PrintWriter outPrintWriter = response.getWriter();
+		outPrintWriter.print("Mensaje: " + mensajeString);
 	}
 
 }
