1842b9b6bdb99b9fb5238ef41301e09392301fc1
xabertum <mohatar@gmail.com>
2019-08-14 12:04:48 +0200

    work done

--- src/cuartoCurso/lecture11/HttpSession/HttpSession.java
+++ src/cuartoCurso/lecture11/HttpSession/HttpSession.java
@@ -13,29 +13,48 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HttpSession")
 public class HttpSession extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HttpSession() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public HttpSession() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		javax.servlet.http.HttpSession session = request.getSession();
+		String tituloString = null;
+
+		// Pedimos el atributo y verficamos si existe
+		Integer contadorVisitas = (Integer) session.getAttribute("contadorVisitas");
+
+		// Si es igual a null, quiere decir que es la primera vez que se accede al
+		// recurso
+		if (contadorVisitas == null) {
+			contadorVisitas = new Integer(1);
+			tituloString = "Bienvenido por primera vez...";
+		} else {
+			// Si es distinto de nulo, incrementamos el contador
+			tituloString = "Bienvenido Nuevamente";
+			contadorVisitas++;
+		}
+
+		// En cualquier caso agregamos el valor del contador a la sesion
+		session.setAttribute("contadorVisitas", contadorVisitas);
+
+		// Mostramos los valores en el cliente
+		response.getWriter().print("Titulo: " + tituloString);
+		response.getWriter().print("Numero de accesos al recurso: " + contadorVisitas);
+		response.getWriter().print("ID de la session: " + session.getId());
+
 	}
 
 }
